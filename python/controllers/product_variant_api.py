9b6a31ee9d4e71e23267a037c7e2de104478b4d7
dung977 <dungh4711@gmail.com>
2026-04-01 14:00:28 +0700

    Update product_variant_api.py

--- python/controllers/product_variant_api.py
+++ python/controllers/product_variant_api.py
@@ -17,18 +17,16 @@ def get_all_variant():
     except Exception as e:
         return flask.jsonify({"error": str(e)}), 500
 
-
 @variant_bp.route('/<ID>', methods=['GET'])
 def get_variant_by_id(ID):
     cursor = conn.cursor()
     try:
-        query = "SELECT * FROM Productvariant pv WHERE pv.ProductVariantID = ?"
+        query = "SELECT * FROM Productvariant WHERE ProductVariantID = ?"
         cursor.execute(query, (ID,))
         res = get_json_results(cursor)
         return flask.jsonify(res), 200
     except Exception as e:
         return flask.jsonify({"error": str(e)}), 500
-
 
 @variant_bp.route('/add', methods=['POST'])
 def add_variant():
@@ -40,31 +38,39 @@ def add_variant():
         StockQuantity = flask.request.json.get("StockQuantity")
         SellingPrice = flask.request.json.get("SellingPrice")
         Description = flask.request.json.get("SellingPrice")
-        cursor.execute("SELECT ProductVariantID FROM ProductVariant WHERE ProductID = ?", (ProductVariantID,))
+        isDelete = 0
+
+        cursor.execute("SELECT ProductVariantID FROM ProductVariant WHERE ProductVariantID = ?", (ProductVariantID,))
         if cursor.fetchone():
             return flask.jsonify({"message": "ProductVariantID already exist!"}), 400
+        cursor.execute("SELECT ProductID FROM Product WHERE ProductID = ?", (ProductID,))
+        if not cursor.fetchone():
+            return flask.jsonify({"message": "Product does not exist!"}), 400
+        
         query = """
                 INSERT INTO ProductVariant(ProductVariantID, ProductID, Color,  
-                SellingPrice, StockQuantity, Description) 
-                VALUES(?, ?, ?, ?, ?, ?, ?)
+                SellingPrice, StockQuantity, Description, isDelete) 
+                VALUES(?, ?, ?, ?, ?, ?, ?, ?)
                 """
-        cursor.execute(query, (ProductVariantID, ProductID, Color, SellingPrice, StockQuantity, Description))
-        conn.commit()
-        
+        cursor.execute(query, (ProductVariantID, ProductID, Color, SellingPrice, StockQuantity, Description, isDelete))
+        conn.commit()   
         return flask.jsonify({"message": "Success!"}), 201
     except Exception as e:
         return flask.jsonify({"error": str(e)}), 500
-
 
 @variant_bp.route('/update/<ID>', methods=['PUT'])
 def update_variant(ID):
     cursor = conn.cursor()
     try:
         ProductID = flask.request.json.get("ProductID")
+        cursor.execute("SELECT ProductID FROM Product WHERE ProductID = ?", (ProductID,))
+        if not cursor.fetchone():
+            return flask.jsonify({"message": "Product does not exist!"}), 400
         Color = flask.request.json.get("Color")
         StockQuantity = flask.request.json.get("StockQuantity")
         SellingPrice = flask.request.json.get("SellingPrice")
         Description = flask.request.json.get("Description")
+
         query = """
                 UPDATE Productvariant SET ProductID = ?, Color = ?,
                 SellingPrice = ?, StockQuantity = ?, Description = ?
@@ -82,7 +88,7 @@ def update_variant(ID):
 def delete_variant(ID):
     cursor = conn.cursor()
     try:
-        query = "DELETE FROM Productvariant WHERE ProductVariantID = ?"
+        query = "UPDATE FROM Productvariant SET isDelete = 1 WHERE ProductVariantID = ?"
         cursor.execute(query, (ID,))
         conn.commit()
         
